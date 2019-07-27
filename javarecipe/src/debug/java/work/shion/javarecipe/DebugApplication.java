7863127d51459b93b09c2e74dde69c5fbefbe0cf
shion <tenta_shion@yahoo.co.jp>
2019-07-27 21:50:37 +0900

    StrictMode の整備

--- javarecipe/src/debug/java/work/shion/javarecipe/DebugApplication.java
+++ javarecipe/src/debug/java/work/shion/javarecipe/DebugApplication.java
@@ -1,8 +1,37 @@
 package work.shion.javarecipe;
 
 
+import work.shion.strictmode.IStrictModeAttacher;
+
 /**
  * デバッグ用アプリ状態の管理
  */
-public class DebugApplication extends MainApplication {
+public class DebugApplication extends MainApplication
+        implements IStrictModeAttacher {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // StrictMode の有効化
+        setupThreadPolicy();
+        setupVmPolicy();
+    }
+
+
+    /**
+     * ThreadPolicy のセットアップ
+     */
+    @Override
+    public void setupThreadPolicy() {
+        IStrictModeAttacher.DefaultImpls.setupThreadPolicy(this);
+    }
+
+    /**
+     * VmPolicy のセットアップ
+     */
+    @Override
+    public void setupVmPolicy() {
+        IStrictModeAttacher.DefaultImpls.setupVmPolicy(this);
+    }
 }
--- ktrecipe/src/debug/java/work/shion/ktrecipe/DebugApplication.kt
+++ ktrecipe/src/debug/java/work/shion/ktrecipe/DebugApplication.kt
@@ -1,8 +1,19 @@
 package work.shion.ktrecipe
 
+import work.shion.strictmode.IStrictModeAttacher
+
 
 /**
  * デバッグ用アプリ状態の管理
  */
-class DebugApplication : MainApplication() {
+class DebugApplication : MainApplication(),
+    IStrictModeAttacher {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // StrictMode の有効化
+        setupThreadPolicy()
+        setupVmPolicy()
+    }
 }
