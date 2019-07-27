a0b3b4ed45473cd81a8dd305062b0f36c3615849
shion <tenta_shion@yahoo.co.jp>
2019-07-27 21:37:55 +0900

    Stetho の導入

--- javarecipe/src/debug/java/work/shion/javarecipe/DebugApplication.java
+++ javarecipe/src/debug/java/work/shion/javarecipe/DebugApplication.java
@@ -1,8 +1,32 @@
 package work.shion.javarecipe;
 
+import android.content.Context;
+import androidx.annotation.NonNull;
+import work.shion.baser.stetho.IStethoAttacher;
+
 
 /**
  * デバッグ用アプリ状態の管理
  */
-public class DebugApplication extends MainApplication {
+public class DebugApplication extends MainApplication
+        implements IStethoAttacher {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Stetho 設定
+        attachStetho(getApplicationContext());
+    }
+
+
+    /**
+     * Stetho の組み込み
+     *
+     * @param appContext アプリケーションコンテキスト
+     */
+    @Override
+    public void attachStetho(@NonNull Context appContext) {
+        IStethoAttacher.DefaultImpls.attachStetho(this, appContext);
+    }
 }
--- ktrecipe/src/debug/java/work/shion/ktrecipe/DebugApplication.kt
+++ ktrecipe/src/debug/java/work/shion/ktrecipe/DebugApplication.kt
@@ -1,8 +1,18 @@
 package work.shion.ktrecipe
 
+import work.shion.baser.stetho.IStethoAttacher
+
 
 /**
  * デバッグ用アプリ状態の管理
  */
-class DebugApplication : MainApplication() {
+class DebugApplication : MainApplication(),
+    IStethoAttacher {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Stetho 設定
+        attachStetho(applicationContext)
+    }
 }
