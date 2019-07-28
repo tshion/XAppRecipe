3ede813a522f5c87439d092c2d537491954cf5cf
shion <tenta_shion@yahoo.co.jp>
2019-07-28 12:11:54 +0900

    Tutorial とTab ページの切り替え機構のサンプル実装

---
+++ ktrecipe/src/main/java/work/shion/ktrecipe/entrypoint/TutorialFragment.kt
@@ -0,0 +1,63 @@
+package work.shion.ktrecipe.entrypoint
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.entrypoint_fragment_tutorial.*
+import work.shion.ktrecipe.R
+
+
+/**
+ * チュートリアル表示用Fragment
+ */
+class TutorialFragment : Fragment() {
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * [.onCreate] and [.onActivityCreated].
+     *
+     *
+     * If you return a View from here, you will later be called in
+     * [.onDestroyView] when the view is being released.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the View for the fragment's UI, or null.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.entrypoint_fragment_tutorial, container, false)
+    }
+
+    /**
+     * Called immediately after [.onCreateView]
+     * has returned, but before any saved state has been restored in to the view.
+     * This gives subclasses a chance to initialize themselves once
+     * they know their view hierarchy has been completely created.  The fragment's
+     * view hierarchy is not however attached to its parent at this point.
+     * @param view The View returned by [.onCreateView].
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        entrypoint_fragment_tutorial_button.setOnClickListener {
+            val target = activity ?: return@setOnClickListener
+            Navigation.findNavController(
+                    target,
+                    R.id.entrypoint_navigation_host_main
+            ).navigate(R.id.entrypoint_action_any_to_tab)
+        }
+    }
+}
