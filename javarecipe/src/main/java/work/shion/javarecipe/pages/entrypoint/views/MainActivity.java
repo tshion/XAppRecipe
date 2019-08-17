9599db20e5240e87da503b707eec10a2d886140d
shion <tenta_shion@yahoo.co.jp>
2019-08-17 11:25:59 +0900

    【Java 版】EntryPoint のメインコンテンツの実装分解

--- javarecipe/src/main/java/work/shion/javarecipe/pages/entrypoint/views/MainActivity.java
+++ javarecipe/src/main/java/work/shion/javarecipe/pages/entrypoint/views/MainActivity.java
@@ -7,16 +7,28 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
+import java.lang.ref.WeakReference;
 import java.util.Date;
 import java.util.Random;
 
 import work.shion.javarecipe.R;
+import work.shion.javarecipe.pages.entrypoint.contracts.MainPresenterContract;
+import work.shion.javarecipe.pages.entrypoint.contracts.MainViewContract;
+import work.shion.javarecipe.pages.entrypoint.models.MainModel;
+import work.shion.javarecipe.pages.entrypoint.presenters.MainPresenter;
 
 
 /**
  * メインコンテンツ表示用Activity
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MainViewContract {
+
+    private final MainPresenterContract presenter = new MainPresenter(
+            new MainModel(),
+            new WeakReference<>(this)
+    );
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,12 +36,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.entrypoint_activity_main);
 
         // 初期表示をランダムで変更する
-        final @IdRes int targetNavigationId = new Random(new Date().getTime()).nextInt(100) < 50
-                ? R.id.entrypoint_action_any_to_tutorial
-                : R.id.entrypoint_action_any_to_tab;
+        presenter.callFirstView();
+    }
+
+
+    /**
+     * Tab ページに遷移
+     */
+    @Override
+    public void goTabPage() {
         Navigation.findNavController(
                 this,
                 R.id.entrypoint_navigation_host_main
-        ).navigate(targetNavigationId);
+        ).navigate(R.id.entrypoint_action_any_to_tab);
+    }
+
+    /**
+     * Tutorial ページに遷移
+     */
+    @Override
+    public void goTutorialPage() {
+        Navigation.findNavController(
+                this,
+                R.id.entrypoint_navigation_host_main
+        ).navigate(R.id.entrypoint_action_any_to_tutorial);
     }
 }
