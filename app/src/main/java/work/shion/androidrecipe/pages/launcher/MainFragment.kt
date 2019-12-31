406c4527a7b8df6b8139570958f042e804041e8d
shion <tenta_shion@yahoo.co.jp>
2019-12-31 22:53:38 +0900

    基礎画面の整備

---
+++ app/src/main/java/work/shion/androidrecipe/pages/launcher/MainFragment.kt
@@ -0,0 +1,6 @@
+package work.shion.androidrecipe.pages.launcher
+
+import androidx.fragment.app.Fragment
+import work.shion.androidrecipe.R
+
+class MainFragment : Fragment(R.layout.launcher)
---
+++ app/src/main/java/work/shion/androidrecipe/views/EntryPointActivity.kt
@@ -0,0 +1,13 @@
+package work.shion.androidrecipe.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import work.shion.androidrecipe.R
+
+class EntryPointActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.entrypoint)
+    }
+}
