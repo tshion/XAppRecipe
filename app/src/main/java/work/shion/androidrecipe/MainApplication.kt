81a70f9cf8696116cc4344af58f8b9145fc0d5e8
shion <tenta_shion@yahoo.co.jp>
2019-12-31 20:02:06 +0900

    ビジネスロジック提供ロジックの実装

--- app/src/main/java/work/shion/androidrecipe/MainApplication.kt
+++ app/src/main/java/work/shion/androidrecipe/MainApplication.kt
@@ -17,5 +17,7 @@ open class MainApplication : Application() {
                 }
             }
         })
+
+        ModelProvider.init()
     }
 }
---
+++ app/src/main/java/work/shion/androidrecipe/ModelProvider.kt
@@ -0,0 +1,37 @@
+package work.shion.androidrecipe
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import work.shion.androidrecipe.repositories.ToDoRepository
+import work.shion.androidrecipe.repositories.api_v1.APIEndpoint
+import work.shion.androidrecipe.usecases.IToDoUseCase
+import work.shion.androidrecipe.usecases.ToDoUseCase
+
+/**
+ * ToDo: DI ライブラリに差し替えたい
+ */
+object ModelProvider {
+
+    lateinit var todoUseCase: IToDoUseCase
+
+
+    fun init() {
+        val apiBuilder = OkHttpClient.Builder()
+                .build()
+                .let {
+                    Retrofit.Builder()
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .baseUrl("http://localhost:8080")
+                            .client(it)
+                            .build()
+                }
+        val apiV1 = apiBuilder.create(APIEndpoint::class.java)
+
+
+        val todoRepository = ToDoRepository(apiV1)
+
+
+        todoUseCase = ToDoUseCase(todoRepository)
+    }
+}
