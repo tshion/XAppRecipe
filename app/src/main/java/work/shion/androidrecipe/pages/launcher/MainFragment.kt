e90bf7782a387eaae7a6bff23e49143f1c151232
shion <tenta_shion@yahoo.co.jp>
2020-01-03 19:17:14 +0900

    アプリ起動処理後の画面遷移追加

--- app/src/main/java/work/shion/androidrecipe/pages/launcher/MainFragment.kt
+++ app/src/main/java/work/shion/androidrecipe/pages/launcher/MainFragment.kt
@@ -1,6 +1,30 @@
 package work.shion.androidrecipe.pages.launcher
 
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.coroutines.*
 import work.shion.androidrecipe.R
+import kotlin.coroutines.CoroutineContext
 
-class MainFragment : Fragment(R.layout.launcher)
+class MainFragment : CoroutineScope, Fragment(R.layout.launcher) {
+
+    private val job = SupervisorJob()
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+
+    override fun onStart() {
+        super.onStart()
+        launch(Dispatchers.Default) {
+            delay(1000)
+            Navigation.findNavController(view!!)
+                    .navigate(R.id.navact_todo_list)
+        }
+    }
+
+    override fun onStop() {
+        coroutineContext.cancelChildren()
+        super.onStop()
+    }
+}
