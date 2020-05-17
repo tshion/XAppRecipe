81d304a53bae6a3169b9224162eceae71a19bc65
shion <tenta_shion@yahoo.co.jp>
2020-05-17 17:20:57 +0900

    【iOS】Xcode Previews の導入

---
+++ ios/App/AppPreview/BridgeVCPreview.swift
@@ -0,0 +1,19 @@
+import Capacitor
+import SwiftUI
+
+struct BridgeVCWrapper: UIViewControllerRepresentable {
+    typealias UIViewControllerType = CAPBridgeViewController
+
+    func makeUIViewController(context: Context) -> CAPBridgeViewController {
+        CAPBridgeViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: CAPBridgeViewController, context: Context) {
+    }
+}
+
+struct BridgeVCWrapper_Previews: PreviewProvider {
+    static var previews: some View {
+        BridgeVCWrapper()
+    }
+}
