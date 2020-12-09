09a1dedb919d3bf946827eafa703e91b01df302d
shion <tenta_shion@yahoo.co.jp>
2020-12-09 20:29:13 +0900

    [droid]ホームの追加

---
+++ android/app/src/main/java/work/shion/xapprecipe/pages/home/MainFragment.kt
@@ -0,0 +1,9 @@
+package work.shion.xapprecipe.pages.home
+
+import androidx.fragment.app.Fragment
+import work.shion.xapprecipe.R
+
+/**
+ * ホーム
+ */
+class MainFragment : Fragment(R.layout.pages_home)
