cbbf0ce8ce960aeed86d4c272b7483c6e116394c
shion <tenta_shion@yahoo.co.jp>
2020-12-09 20:28:35 +0900

    [droid]通知一覧の追加

---
+++ android/app/src/main/java/work/shion/xapprecipe/pages/notice/MainFragment.kt
@@ -0,0 +1,9 @@
+package work.shion.xapprecipe.pages.notice
+
+import androidx.fragment.app.Fragment
+import work.shion.xapprecipe.R
+
+/**
+ * 通知一覧
+ */
+class MainFragment : Fragment(R.layout.pages_notice)
