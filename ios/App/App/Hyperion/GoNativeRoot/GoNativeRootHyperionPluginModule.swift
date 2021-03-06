43237c2a0cafe24a8f49ddba51ea05ffa8a5b308
shion <tenta_shion@yahoo.co.jp>
2021-03-06 19:08:59 +0900

    画面閉じる導線の追加

--- ios/App/App/Hyperion/GoNativeRoot/GoNativeRootHyperionPluginModule.swift
+++ ios/App/App/Hyperion/GoNativeRoot/GoNativeRootHyperionPluginModule.swift
@@ -2,6 +2,8 @@ import Foundation
 import HyperionCore
 
 class GoNativeRootHyperionPluginModule: HYPPluginModule {
+    private var displayingViewController: UIViewController?
+
     required init(with ext: HYPPluginExtensionProtocol) {
         super.init(with: ext)
     }
@@ -17,9 +19,23 @@ extension GoNativeRootHyperionPluginModule: HYPPluginMenuItemDelegate {
             return
         }
 
-        let nextPage = NativeRootViewController.newInstance()
-        nextPage.modalPresentationStyle = .fullScreen
+        let buttonClose = UIButton(type: UIButton.ButtonType.detailDisclosure)
+        buttonClose.addTarget(self, action: #selector(tappedButtonClose(_:)), for: UIControl.Event.touchUpInside)
+        buttonClose.backgroundColor = UIColor.systemOrange
+        buttonClose.sizeToFit()
 
+        let nextPage = NativeRootViewController.newInstance()
+               
+        buttonClose.center = CGPoint(x: 16, y: nextPage.view.center.y)
+        nextPage.view.addSubview(buttonClose)
+
+        nextPage.modalPresentationStyle = .fullScreen
         nowViewController.present(nextPage, animated: true, completion: nil)
+        displayingViewController = nextPage
+    }
+
+    @objc func tappedButtonClose(_ sender: UIButton) {
+        displayingViewController?.dismiss(animated: true, completion: nil)
+        displayingViewController = nil
     }
 }
