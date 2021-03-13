103d24cc4e7dc1eb97fbc99b972ea6c19ae327d5
shion <tenta_shion@yahoo.co.jp>
2021-03-13 19:20:13 +0900

    [iOS]Web 表示の仮実装

--- ios/App/App/Pages/BrowseWeb/BrowseWebViewController.swift
+++ ios/App/App/Pages/BrowseWeb/BrowseWebViewController.swift
@@ -1,14 +1,21 @@
-import UIKit
+import WebKit
 
 /**
  アプリ内WEB 表示
  */
 class BrowseWebViewController: UIViewController {
+    @IBOutlet weak var browser: WKWebView!
+
     static func newInstance() -> BrowseWebViewController {
         return BrowseWebViewController(nibName: "BrowseWebViewController", bundle: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if let url = URL(string: "https://www.google.co.jp/maps") {
+            let request = URLRequest(url: url)
+            browser.load(request)
+        }
     }
 }
