44d5f133ce01a3f85a3c2696d19f4344b022fcbd
shion <tenta_shion@yahoo.co.jp>
2021-05-02 08:57:08 +0900

    [droid]ログアウト確認ダイアログの追加

---
+++ android/app/src/main/java/work/shion/xapprecipe/templates/logout_confirm_dialog/LogoutConfirmDialog.kt
@@ -0,0 +1,54 @@
+package work.shion.xapprecipe.templates.logout_confirm_dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.activityViewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import work.shion.xapprecipe.R
+
+/**
+ * ログアウト確認ダイアログ
+ *
+ * ## Example
+ * ### ダイアログ選択結果の受け取り
+ * ``` kotlin
+ * class Xxx : Fragment() {
+ *     private val logoutConfirmDialogViewModel by activityViewModels<LogoutConfirmDialogViewModel>()
+ *
+ *     ......
+ *
+ *     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+ *         super.onViewCreated(view, savedInstanceState)
+ *
+ *         logoutConfirmDialogViewModel.isCalledDismiss.observe(viewLifecycleOwner) {
+ *             if (it) {
+ *                 logoutConfirmDialogViewModel.isCalledRetry.value = false
+ *             }
+ *         }
+ *     }
+ * }
+ * ```
+ */
+class LogoutConfirmDialog : DialogFragment() {
+
+    private val viewModel by activityViewModels<LogoutConfirmDialogViewModel>()
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return MaterialAlertDialogBuilder(requireContext(), R.style.TemplatesLogoutConfirmDialog)
+            .setMessage(R.string.templates_logout_confirm_dialog_message)
+            .setNegativeButton(R.string.templates_logout_confirm_dialog_negative, null)
+            .setPositiveButton(R.string.templates_logout_confirm_dialog_positive) { _, _ ->
+                viewModel.isCalledDismiss.value = true
+                dismiss()
+            }
+            .setTitle(R.string.templates_logout_confirm_dialog_title)
+            .create()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dismiss()
+    }
+}
