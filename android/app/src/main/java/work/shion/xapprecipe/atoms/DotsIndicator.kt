9aced78ce452dd6a6a0b0d0d25c73546accf97f5
shion <tenta_shion@yahoo.co.jp>
2021-05-03 15:36:09 +0900

    [droid]ドキュメントの追加

--- android/app/src/main/java/work/shion/xapprecipe/atoms/DotsIndicator.kt
+++ android/app/src/main/java/work/shion/xapprecipe/atoms/DotsIndicator.kt
@@ -8,6 +8,32 @@ import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
 import work.shion.xapprecipe.databinding.AtomsDotsIndicatorBinding
 
+/**
+ * 点のインジケーター
+ *
+ * ## Example
+ * ``` xml
+ * <androidx.viewpager2.widget.ViewPager2
+ *     android:id="(id_pager)"
+ *     android:layout_width="match_parent"
+ *     android:layout_height="0dp"
+ *     android:layout_weight="1" />
+ *
+ * <(package).atoms.DotsIndicator
+ *     android:id="(id_dots)"
+ *     android:layout_width="match_parent"
+ *     android:layout_height="wrap_content" />
+ * ```
+ *
+ * ``` kotlin
+ * val dots = findViewById<DotsIndicator>(R.id.(id_dots))
+ * val pager = findViewById<ViewPager2>(R.id.(id_pager))
+ *
+ * pager.adapter = (Adapter)
+ * TablayoutMediator(dots, pager) { tab, position ->
+ * }.attach()
+ * ```
+ */
 class DotsIndicator @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
