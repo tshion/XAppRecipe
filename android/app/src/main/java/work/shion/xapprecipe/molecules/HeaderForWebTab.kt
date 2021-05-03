b1cd3a5005dd8705205e57df23cceefd9453d405
shion <tenta_shion@yahoo.co.jp>
2021-05-03 15:13:05 +0900

    [droid]ブラッシュアップ

--- android/app/src/main/java/work/shion/xapprecipe/molecules/HeaderForWebTab.kt
+++ android/app/src/main/java/work/shion/xapprecipe/molecules/HeaderForWebTab.kt
@@ -3,7 +3,8 @@ package work.shion.xapprecipe.molecules
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.annotation.AttrRes
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.withStyledAttributes
 import work.shion.xapprecipe.R
 import work.shion.xapprecipe.databinding.MoleculesHeaderForWebTabBinding
@@ -13,7 +14,7 @@ import work.shion.xapprecipe.databinding.MoleculesHeaderForWebTabBinding
  *
  * ## Example1
  * ``` xml
- * <(package).molecules.job_overview.HeaderForWebTab
+ * <(package).molecules.HeaderForWebTab
  *     android:id="@+id/(id)"
  *     android:layout_width="match_parent"
  *     android:layout_height="?attr/actionBarSize"
@@ -31,9 +32,8 @@ import work.shion.xapprecipe.databinding.MoleculesHeaderForWebTabBinding
 class HeaderForWebTab @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0,
-    defStyleRes: Int = R.style.molecules_header_for_web_tab,
-) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+    @AttrRes defStyleAttr: Int = 0,
+) : LinearLayoutCompat(context, attrs, defStyleAttr) {
 
     private val binding = MoleculesHeaderForWebTabBinding.inflate(
         LayoutInflater.from(context),
@@ -42,11 +42,13 @@ class HeaderForWebTab @JvmOverloads constructor(
 
 
     init {
+        orientation = HORIZONTAL
+        setBackgroundResource(R.color.app_main)
+
         context.withStyledAttributes(
             attrs,
             R.styleable.HeaderForWebTab,
             defStyleAttr,
-            defStyleRes,
         ) {
             binding.moleculesHeaderForWebTabTitle.text = getString(R.styleable.HeaderForWebTab_android_text)
         }
