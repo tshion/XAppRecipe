73c4a95ebe29d46eea64c40ccd7157f22302e1f8
shion <tenta_shion@yahoo.co.jp>
2021-05-06 14:01:46 +0900

    [droid]ブラッシュアップ

--- android/app/src/main/java/work/shion/xapprecipe/pages/top/MainFragment.kt
+++ android/app/src/main/java/work/shion/xapprecipe/pages/top/MainFragment.kt
@@ -37,7 +37,11 @@ class MainFragment : Fragment(), MainViewContract {
     }
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = PagesTopBinding.inflate(inflater, container, false)
         return binding?.root
     }
