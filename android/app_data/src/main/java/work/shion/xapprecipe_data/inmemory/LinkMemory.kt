d20e0754d3c5fb7b99a834b7f8f00a86bbcdf250
shion <tenta_shion@yahoo.co.jp>
2021-05-07 20:44:30 +0900

    [droid]インメモリの追加

---
+++ android/app_data/src/main/java/work/shion/xapprecipe_data/inmemory/LinkMemory.kt
@@ -0,0 +1,27 @@
+package work.shion.xapprecipe_data.inmemory
+
+import work.shion.xapprecipe_core.entities.WebLinkEntity
+
+/**
+ * リンク情報の保存実装(インメモリ)
+ */
+class LinkMemory : LinkMemoryContract {
+
+    val store = arrayListOf<WebLinkEntity>()
+
+
+    override fun load() = store
+
+    override fun remove(id: String) {
+        store.removeAll { it.id == id }
+    }
+
+    override fun upsert(target: WebLinkEntity) {
+        val index = store.indexOfFirst { it.id == target.id }
+        if (0 <= index) {
+            store[index] = target
+        } else {
+            store.add(target)
+        }
+    }
+}
