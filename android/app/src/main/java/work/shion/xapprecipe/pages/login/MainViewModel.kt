4a5746d97c0cc41ddb0602f7526da0568e6cbca1
shion <tenta_shion@yahoo.co.jp>
2021-05-08 14:05:55 +0900

    [droid]挙動の整備

--- android/app/src/main/java/work/shion/xapprecipe/pages/login/MainViewModel.kt
+++ android/app/src/main/java/work/shion/xapprecipe/pages/login/MainViewModel.kt
@@ -1,9 +1,18 @@
 package work.shion.xapprecipe.pages.login
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import work.shion.xapprecipe.R
+import work.shion.xapprecipe_core.entities.LoginEntity
+import work.shion.xapprecipe_core.errors.LoginException
+import work.shion.xapprecipe_core.usecases.CertifyAccountUseCaseContract
 import java.lang.ref.WeakReference
 
 class MainViewModel(
+    private val certifyAccountUseCaseContract: CertifyAccountUseCaseContract,
+    private val context: WeakReference<Context>,
     private val viewer: WeakReference<MainViewContract>
 ) : ViewModel(), MainActionContract {
 
@@ -11,11 +20,34 @@ class MainViewModel(
      * ログインの実行
      */
     override fun doLogin(id: String?, pw: String?) {
-        // TODO: ログイン処理
-        viewer.get()?.reflectError(
-            idError = "ID: $id の判定は未実装",
-            pwError = "PW: $pw の判定は未実装",
-        )
+        viewModelScope.launch {
+            try {
+                LoginEntity(id, pw)
+                    .also { certifyAccountUseCaseContract.login(it) }
+                viewer.get()?.goTop()
+            } catch (error: LoginException) {
+                viewer.get()?.reflectError(
+                    idError = error.idErrors?.mapNotNull {
+                        when (it) {
+                            is IllegalArgumentException -> context.get()
+                                ?.getString(R.string.pages_login_error_id_invalid)
+                            is NullPointerException -> context.get()
+                                ?.getString(R.string.pages_login_error_empty)
+                            else -> null
+                        }
+                    }?.joinToString(separator = "") { "・${it}\n" },
+                    pwError = error.pwErrors?.mapNotNull {
+                        when (it) {
+                            is IllegalArgumentException -> context.get()
+                                ?.getString(R.string.pages_login_error_pw_invalid)
+                            is NullPointerException -> context.get()
+                                ?.getString(R.string.pages_login_error_empty)
+                            else -> null
+                        }
+                    }?.joinToString(separator = "") { "・${it}\n" },
+                )
+            }
+        }
     }
 
     /**
