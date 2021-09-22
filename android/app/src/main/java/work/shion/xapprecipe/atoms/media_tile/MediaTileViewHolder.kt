6d3ab9ca953dd88eb517bc6dc2c88438f0a08a5b
shion <tenta_shion@yahoo.co.jp>
2021-09-22 22:42:47 +0900

    [droid]メディア表示タイルの追加

---
+++ android/app/src/main/java/work/shion/xapprecipe/atoms/media_tile/MediaTileViewHolder.kt
@@ -0,0 +1,7 @@
+package work.shion.xapprecipe.atoms.media_tile
+
+import androidx.recyclerview.widget.RecyclerView
+
+class MediaTileViewHolder(
+    val root: MediaTile
+) : RecyclerView.ViewHolder(root)
