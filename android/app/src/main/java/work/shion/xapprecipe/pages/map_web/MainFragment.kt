82e1c52a1fd1e78455a25409e24a5fef3873a4b1
shion <tenta_shion@yahoo.co.jp>
2022-02-16 15:32:46 +0900

    WebViewBuilder の差し替え

--- android/app/src/main/java/work/shion/xapprecipe/pages/map_web/MainFragment.kt
+++ android/app/src/main/java/work/shion/xapprecipe/pages/map_web/MainFragment.kt
@@ -7,10 +7,10 @@ import android.view.ViewGroup
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.github.tshion.mktools_android.webview_builder.WebViewBuilder
 import permissions.dispatcher.ktx.LocationPermission
 import permissions.dispatcher.ktx.constructLocationPermissionRequest
 import work.shion.androidpreparation.intentbuilder.LaunchPhoneIntentBuilder
-import work.shion.androidpreparation.webviewbuilder.WebViewBuilder
 import work.shion.xapprecipe.BuildConfig
 import work.shion.xapprecipe.databinding.PagesMapWebBinding
 import work.shion.xapprecipe.pages.top.MainFragment as TopFragment
@@ -55,22 +55,22 @@ class MainFragment : Fragment() {
             .onGeolocationPermissionsShowPrompt { origin, callback ->
                 constructLocationPermissionRequest(
                     LocationPermission.FINE,
-                    onNeverAskAgain = { callback?.invoke(origin, false, false) },
-                    onPermissionDenied = { callback?.invoke(origin, false, false) },
+                    onNeverAskAgain = { callback.invoke(origin, false, false) },
+                    onPermissionDenied = { callback.invoke(origin, false, false) },
                     onShowRationale = { it.proceed() },
                 ) {
-                    callback?.invoke(origin, true, false)
+                    callback.invoke(origin, true, false)
                 }.launch()
             }
             .shouldOverrideUrlLoading { _, request ->
-                if (request?.url?.scheme?.equals("tel", true) == true) {
+                if (request.url?.scheme?.equals("tel", true) == true) {
                     val phoneNumber = request.url.toString().removePrefix("tel:")
                     LaunchPhoneIntentBuilder()
                         .phoneNumber(phoneNumber)
                         .build()
                         .also { startActivity(it) }
                 } else {
-                    request?.url?.also {
+                    request.url?.also {
                         CustomTabsIntent.Builder()
                             .build()
                             .launchUrl(requireContext(), it)
