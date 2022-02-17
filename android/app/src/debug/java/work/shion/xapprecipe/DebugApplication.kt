27d5c53dfad32d4698a4bd0f54dc6ab5d0264b7b
shion <tenta_shion@yahoo.co.jp>
2022-02-17 16:25:49 +0900

    VmPolicy のペナルティ調整
    他プロセスの違反検知が多いため

--- android/app/src/debug/java/work/shion/xapprecipe/DebugApplication.kt
+++ android/app/src/debug/java/work/shion/xapprecipe/DebugApplication.kt
@@ -1,11 +1,19 @@
 package work.shion.xapprecipe
 
+import android.os.StrictMode.VmPolicy
 import com.github.tshion.mktools_android.StrictModeActivator
 import work.shion.androidpreparation.debugger.android.IWebViewAttacher
 import work.shion.androidpreparation.debugger.stetho.IStethoAttacher
 
 class DebugApplication : MainApplication(),
     IStethoAttacher, IWebViewAttacher, StrictModeActivator {
+
+    override val vmPolicy: VmPolicy
+        get() = VmPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+            .build()
+
 
     override fun onCreate() {
         super.onCreate()
