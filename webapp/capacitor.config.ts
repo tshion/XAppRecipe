5c06dc6468da43264f4673c347e66030ad6dd319
shion <tenta_shion@yahoo.co.jp>
2022-06-12 17:29:04 +0900

    WEB 関連の初期配置

---
+++ webapp/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.github.tshion.xapprecipe.webapp',
+  appName: 'webapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
