dec3779ca55bb626886a7c45cff4dc3eb9969213
shion <tenta_shion@yahoo.co.jp>
2022-08-06 16:14:24 +0900

    旧レシピ実装の削除

--- old_recipe/app/src/main/java/work/shion/androidrecipe/pages/todo_list/ItemViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package work.shion.androidrecipe.pages.todo_list
-
-import androidx.recyclerview.widget.RecyclerView
-import work.shion.androidrecipe.databinding.TodoListItemBinding
-
-class ItemViewHolder(
-        val binding: TodoListItemBinding
-) : RecyclerView.ViewHolder(binding.root)
--- old_recipe/app/src/main/java/work/shion/androidrecipe/views/EntryPointActivity.kt
+++
@@ -1,3 +0,0 @@
-package work.shion.androidrecipe.views
-
-class EntryPointActivity : work.shion.androidpreparation.baser.EntryPointActivity()
